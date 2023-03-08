57b56a2cc1a94deececcd42303f07771e9c0cf03
s25008 <s25008@pjwstk.edu.pl>
2023-03-08 10:05:56 +0100

    Crawler first try

--- Zad1_Crawler/Zad1_Crawler/Program.cs
+++ Zad1_Crawler/Zad1_Crawler/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if(args.Length == 0)
+            {
+                throw new ArgumentNullException();
+            }
+            string url = args[0];
+
+            using HttpClient httpclient = new ();
+
+            HttpResponseMessage result = await httpclient.GetAsync(url);
+
+
+
+
         }
     }
 }
