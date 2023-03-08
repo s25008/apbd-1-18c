bb244bfd0cf08f7f590cc16c6137ce67f37b0a4b
s25008 <s25008@pjwstk.edu.pl>
2023-03-08 09:37:23 +0100

    init

---
+++ Zad1_Crawler/Zad1_Crawler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zad1_Crawler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
